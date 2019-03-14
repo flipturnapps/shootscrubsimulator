a0139c7a41a9cad7664e9f7d87bfb65deb1528b0
Kevin Kellar <kellar.kevin@gmail.com>
2019-03-13 23:58:09 -0700

    adddd

--- src/com/github/kkevlar/scrubshootsim/server/ShootServerMain.java
+++ src/com/github/kkevlar/scrubshootsim/server/ShootServerMain.java
@@ -44,7 +44,7 @@ public class ShootServerMain
         			i++);
         	s.sentToAll(scrubString);
         	try {
-				Thread.sleep(50);
+				Thread.sleep(150);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
